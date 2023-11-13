c4ab325b32b6d1ed25c3e50be9196659dccddfce
Antoine Dangeard <antoine.dangeard@gmail.com>
2023-11-13 02:43:03 -0500

    ready for overnight

--- play.py
+++ play.py
@@ -12,27 +12,6 @@ if __name__ == '__main__':
     agent.p_random_action = 0.0
     agent.explore_unseen_states = False
     
-    seed = 3623
-    ale = makeEnvironment(render=True, seed=seed) #3623 1330
-    
-    possible_actions = [2,3,4,5]
-
-    total_reward = 0
-    action = None
-    
-    while not ale.game_over():
-        state = buildStateFromRAM(ale.getRAM(), action)
-        action, _ = agent.getAction(state)
-        reward = ale.act(action)
-        total_reward += reward
-
-        print("Seed {} - Total accumulated reward: {}".format(seed, total_reward))
-    appendToFile("seed {}, reward {}".format(seed, total_reward), "results.csv")
-    ################################################
-    agent = loadFromPickle("data/checkpoints/best_agent.pkl")
-    agent.p_random_action = 0.0
-    agent.explore_unseen_states = False
-    
     seed = 1330
     ale = makeEnvironment(render=True, seed=seed) #3623 1330
     
