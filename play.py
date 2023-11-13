85884958c6bdac5f8976c6a2215ee1ce083d471f
Antoine Dangeard <antoine.dangeard@gmail.com>
2023-11-13 02:25:37 -0500

    add play script and results

--- play.py
+++ play.py
@@ -7,22 +7,24 @@ from utils import *
 warnings.filterwarnings("ignore")
 
 if __name__ == '__main__':
-    agent = loadFromPickle("data/checkpoints/best_agent.pkl")
-    agent.p_random_action = 0.0
-    agent.explore_unseen_states = False
-    # seed = random.randint(0, 9999)
-    seed = 1330
-    ale = makeEnvironment(render=True, seed=seed) #3623 1330
-    
-    possible_actions = [2,3,4,5]
+    while True:
+        agent = loadFromPickle("data/checkpoints/best_agent.pkl")
+        agent.p_random_action = 0.0
+        agent.explore_unseen_states = False
+        
+        seed = int(input("Enter seed: "))
+        ale = makeEnvironment(render=True, seed=seed) #3623 1330
+        
+        possible_actions = [2,3,4,5]
 
-    total_reward = 0
-    action = None
-    
-    while not ale.game_over():
-        state = buildStateFromRAM(ale.getRAM(), action)
-        action, _ = agent.getAction(state)
-        reward = ale.act(action)
-        total_reward += reward
+        total_reward = 0
+        action = None
+        
+        while not ale.game_over():
+            state = buildStateFromRAM(ale.getRAM(), action)
+            action, _ = agent.getAction(state)
+            reward = ale.act(action)
+            total_reward += reward
 
-        print("Seed {} - Total accumulated reward: {}".format(seed, total_reward))
+            print("Seed {} - Total accumulated reward: {}".format(seed, total_reward))
+        appendToFile("seed {}, reward {}".format(seed, total_reward), "results.csv")
